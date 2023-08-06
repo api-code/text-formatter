db227df3733822e17385f756b27bacc2804dbbce
api-code <madavi.apekshas@gmail.com>
2023-08-07 00:35:17 +0530

    prttier chnages

--- src/App.js
+++ src/App.js
@@ -9,39 +9,43 @@ import Alert from "./components/alert";
 
 function App() {
   const [mode, setMode] = useState("light"); //whearther dark mode is enable or not
-  const[alert, setAlert] = useState(null);
+  const [alert, setAlert] = useState(null);
 
-  const showAlert=(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg : message,
-      type : type
-    })
-    setTimeout(() =>{
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
-    },10000);
-  }
+    }, 1500);
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
-      document.body.style.background ='#042743';
+      document.body.style.background = "#042743";
       showAlert("Dark Mode has been enabled", "Success");
-    } 
-    else {
+    } else {
       setMode("light");
-      document.body.style.background ='white';
+      document.body.style.background = "white";
       showAlert("light Mode has been enabled", "Success");
     }
   };
+
   return (
     //jsx is start from here
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
 
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter Text to analyze ..." mode={mode}></TextForm>
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter Text to analyze below..."
+          mode={mode}
+        ></TextForm>
       </div>
       <div>{/* <About></About> */}</div>
     </>
